8868af1a3e3b236fb3e46a64f02c90fd15ed842a
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2021-03-04 19:16:26 -0500

    Print Function Corrected and Updated
    The print function has been updated and is working as intended.

--- js/script.js
+++ js/script.js
@@ -18,37 +18,43 @@ let quotes = [
     quote: "Far better it is to dare mighty things, to win glorious triumphs, even though checkered by failure, than to take rank with those poor spirits who neither enjoy much nor suffer much, because they live in the gray twilight that knows not victory nor defeat.",
     source: "President Theodore Roosevelt",
     citation: "The Strenuous Life",
-    year: 1899
+    year: 1899,
+    tags: 'Politics'
   },
   {
     quote: "We have always held to the hope, the belief, the conviction that there is a better life, a better world, beyond the horizon.",
     source: "President Franklin Roosevelt", 
     citation: "Address on Hemispheric Defense",
-    year: 1940
+    year: 1940,
+    tags: 'Politics'
   },
   {
     quote: "Don’t ever let somebody tell you you can’t do something, not even me. Alright? You dream, you gotta protect it. People can’t do something themselves, they wanna tell you you can’t do it. If you want something, go get it. Period.",
     source: "Chris Gardner", 
     citation: "The Pursuit of Happyness",
-    year: 2006
+    year: 2006,
+    tags: 'Movies'
   },
   {
     quote: "Many things are formidable, but none are more formidable than man.",
     source: "Sophocles", 
     citation: "Ode to Man in Antigone",
-    year: "441 BC"
+    year: "441 BC",
+    tags: "Literature"
   },
   {
     quote: "Life's not about how hard of a hit you can give... it's about how many you can take, and still keep moving forward.",
     source: "Rocky", 
     citation: "Rocky Balboa",
-    year: 2006
+    year: 2006,
+    tags: 'Movies'
   },
   {
     quote: "The people who are crazy enough to think that they can change the world, are the ones who do.",
     source: "Steve Jobs", 
     citation: "Apple's Think Different Campaign",
-    year: 1997
+    year: 1997,
+    tags: 'Business, Technology'
   }
 ];
 
@@ -64,6 +70,7 @@ console.log(quotes);
 
 let quoteNumber = 0;
 
+
 function getRandomQuote(quotearray) {
   quoteNumber = Math.floor(Math.random()*6);
   //console.log(quoteNumber);
@@ -75,17 +82,33 @@ function getRandomQuote(quotearray) {
 /***
  * `printQuote` function
 ***/
-let random_quote = '';
-let print_html = '';
+/* Cesar's Note: I am declaring the variables outside of the function (need to check if this is necessary). Inside the function
+I am calling the getRandomQuote function to return an object from the quotes array. I am then concatenating object pieces to the
+print_html variable, so that I may return it and write it to the website.
+*/ 
+
+let quote_object;
+let html_print;
 
 function printQuote() {
-  random_quote = getRandomQuote();
-  print_html = `<p>${random_quote[quote]} -${random_quote[source]}</p>`;
+  quote_object = getRandomQuote(quotes);
+  html_print= `
+    <p class="quote"> ${quote_object.quote} </p>
+    <p class="source"> ${quote_object.source}`;
+  //console.log(html_print);
+  if ( quote_object.citation ) {
+    html_print += `
+      <span class="citation"> ${quote_object.citation} </span>`;
+  }
+  if ( quote_object.year ) {
+    html_print += `
+      <span class="year"> ${quote_object.year} </span>`;
+  }
+  html_print += `</p>`;
 
-
-
+  console.log(html_print);
+  
 }
-
 
 
 /***
